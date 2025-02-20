5a0fd6b46cea9e79ef27e967824c923d2894d62e
seunggyun <asg5450@gmail.com>
2025-02-20 11:44:49 +0900

    채팅 상세 조회 구현
    채팅방 내용 반환

--- controller/chat.js
+++ controller/chat.js
@@ -10,3 +10,41 @@ export async function findChatsByUserObjectId(req, res, next) {
   );
   return res.json({ status: true, data: chatList });
 }
+
+export async function findChatsDetailByChatObjectId(req, res, next) {
+  const { objectId } = req.body;
+  console.log("findChatsDetailByChatObjectId() objectId: ", objectId);
+  try {
+    const chat = await chatRepository.findChatById(objectId);
+    if (chat) return res.json({ status: true, data: chat });
+    else
+      return res.json({
+        status: false,
+        message: "해당 ObjectId의 채팅이 존재하지 않습니다.",
+      });
+  } catch (e) {
+    console.log(
+      "controller / chat.js / findChatsDetailByChatObjectId / chatRepository.findChatById() catch",
+      e
+    );
+  }
+}
+
+export async function createChat(req, res, next) {
+  const { objectId, user_model } = req.payload;
+  const { title, messages } = req.body;
+  try {
+    const newChat = await chatRepository.createChat(
+      objectId,
+      user_model,
+      title,
+      messages
+    );
+    return res.json({ status: true, data: newChat });
+  } catch (e) {
+    return res.json({
+      status: false,
+      message: "createChat 수행 중 Exception 발생",
+    });
+  }
+}
