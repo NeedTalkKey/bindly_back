164f07b1ffddc549a6e23c8946e074472e2c746b
박건우 <woorl201362@gmail.com>
2025-02-26 06:44:20 +0900

    호스트 설정
    호스트 설정

--- merged_fastapi.py
+++ merged_fastapi.py
@@ -97,4 +97,4 @@ def predict_intimacy(data: TextInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="3.36.73.192", port=8000)
